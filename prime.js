c202fee83955f9e97ed0c74e15aa6b29d78510e1
abhishekyadav0888 <shraddhayadav0888.com>
2022-11-03 14:59:36 +0530

    I done code.

--- prime.js
+++ prime.js
@@ -2,14 +2,14 @@ function checkprime(num) {
 
 let factor=0;
 	for (let i=1; i<=num; i++) {
-	if(i % 2 = 0) {
+	if(num % i == 0) {
 		factor++;
 	}
 }
-	if (factor == 0) {
+	if (factor == 2) {
 	console.log(num, "is a prime number");
 	} else {
 	console.log(num, "is not a prime number.");
 	}
 
-checkprime(39);
+checkprime(31);
